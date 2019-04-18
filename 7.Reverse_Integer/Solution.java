752195247d90e5aa6cb34ef0945a83e62d47eb43
dhuixing <denghuixing@live.com>
2019-04-18 22:21:15 +0800

    7.reverse integer

---
+++ 7.Reverse_Integer/Solution.java
@@ -0,0 +1,6 @@
+import java.*
+class Solution{
+    public void static main(String[] args){
+        System.out.println("hello");
+    }
+}
---
+++ leetcode/src/Reverse_Integer7/Solution.java
@@ -0,0 +1,7 @@
+package Reverse_Integer7;
+
+public class Solution {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
