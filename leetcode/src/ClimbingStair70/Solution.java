14a1abf441362aa73328a62b9de2ae41b01990b7
dhuixing <denghuixing@live.com>
2019-04-27 11:37:41 +0800

    70 Climbling Stairs

---
+++ leetcode/src/ClimbingStair70/Solution.java
@@ -0,0 +1,27 @@
+package ClimbingStair70;
+
+public class Solution {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        for (int i=1; i<10; i++){
+            System.out.println(s.climbStairs(i));
+        }
+
+    }
+
+    public int climbStairs(int n) {
+        if(n<1) return 0;
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        int res = 0;
+        int tmp1 = 1;
+        int tmp2 = 2;
+        for(int i=3; i<=n;i++){
+            res = tmp1 + tmp2;
+            tmp1 = tmp2;
+            tmp2 = res;
+        }
+        return res;
+    }
+}
