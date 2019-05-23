655e4b46906c879aa492c5796b7d7fdeff69a052
dhuixing <denghuixing@live.com>
2019-05-23 08:15:42 +0800

    init 110 Banlanced Binary Tree

---
+++ leetcode/src/Balanced_Binary_Tree_110/Solution.java
@@ -0,0 +1,24 @@
+package Balanced_Binary_Tree_110;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+
+    public String toString(){
+        return this.val+"";
+    }
+}
+
+public class Solution {
+    public static void main(String[] args){
+        Solution s = new Solution();
+
+    }
+    public boolean isBalanced(TreeNode root) {
+
+    }
+
+}
