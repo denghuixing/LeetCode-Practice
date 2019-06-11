023d546e504faa1c980f49645baa4f0a8ddfdaca
dhuixing <denghuixing@live.com>
2019-06-11 23:25:12 +0800

    finish 122 BestTime to Buy and Sell Stock II

--- leetcode/src/Best_Time_To_Buy_or_Sell_Stock_II_122/Solution.java
+++ leetcode/src/Best_Time_To_Buy_or_Sell_Stock_II_122/Solution.java
@@ -4,10 +4,25 @@ public class Solution {
     public static void main(String[] args){
         Solution s = new Solution();
         int[] stockArr = new int[]{7,1,5,3,6,4};
+        int[] stockArr2 = new int[]{1,2,3,4,5};
         System.out.println(s.maxProfit(stockArr));
     }
-
+    //One pass through method
     public int maxProfit(int[] prices) {
-
+        if(prices.length<1) return 0;
+        int maxProf = 0;
+        int bottom = prices[0];
+        int peak = prices[0];
+        for(int i=1; i<prices.length; i++){
+            if(prices[i]>prices[i-1]){
+                peak = prices[i];
+            }else {
+                maxProf += peak-bottom;
+                bottom = prices[i];
+                peak= prices[i];
+            }
+        }
+        maxProf += peak-bottom;
+        return maxProf;
     }
 }
