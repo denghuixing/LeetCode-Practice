25e7b6a41fd22036805cc1e72c1b08623a9263a3
dhuixing <denghuixing@live.com>
2019-06-12 23:05:13 +0800

    init 135 Single number

---
+++ leetcode/src/Single_Number_136/Solution.java
@@ -0,0 +1,13 @@
+package Single_Number_136;
+
+public class Solution {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int[] nums = new int[]{2,2,1};
+        System.out.println(s.singleNumber());
+    }
+
+    public int singleNumber(int[] nums) {
+
+    }
+}
--- leetcode/src/Valid_palidrome_125/Solution.java
+++ leetcode/src/Valid_palidrome_125/Solution.java
@@ -4,15 +4,26 @@ public class Solution {
     public static void main(String[] args){
         Solution s = new Solution();
         String str = "A man, a plan, a canal: Panama";
-        System.out.println(s.isPalindrome(str));
+        String str2 = "0P";
+        System.out.println(s.isPalindrome(str2));
     }
 
     public boolean isPalindrome(String s) {
-        String reverse = s.toLowerCase().replaceAll(" ","");
-        System.out.println(reverse);
+        if(s.length()<1) return true;
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
         for(char c : s.toCharArray()){
-            if(c>)
-            System.out.println(c);
+            if(Character.isLetter(c)||Character.isDigit(c)){
+                sb.append(c);
+            }
+        }
+        System.out.println(sb);
+        if(sb.length()<=1) return true;
+//        System.out.println(sb);
+        for(int i=0; i<=sb.length()/2; i++){
+            if(sb.charAt(i)!=sb.charAt(sb.length()-1-i)){
+                return false;
+            }
         }
         return true;
     }
