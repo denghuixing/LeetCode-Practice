31f8e37419a71992dd8152a8e8553a6b2d9d37b5
dhuixing <denghuixing@live.com>
2019-06-13 23:28:04 +0800

    init 141 Linked List Cycle

---
+++ leetcode/src/Linked_List_Cycle_141/Solution.java
@@ -0,0 +1,21 @@
+package Linked_List_Cycle_141;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+      val = x;
+      next = null;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        System.out.println(s.hasCycle());
+    }
+
+    public boolean hasCycle(ListNode head) {
+
+    }
+}
