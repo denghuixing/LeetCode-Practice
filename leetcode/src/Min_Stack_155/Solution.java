b253d49b61dbc8b01ceedf39fd676c3abdef6821
dhuixing <denghuixing@live.com>
2019-06-14 21:16:04 +0800

    slove 155 Min Stack

--- leetcode/src/Min_Stack_155/Solution.java
+++ leetcode/src/Min_Stack_155/Solution.java
@@ -1,5 +1,8 @@
 package Min_Stack_155;
 
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class Solution {
     public static void main(String[] args){
         MinStack minStack = new MinStack();
@@ -13,19 +16,26 @@ class MinStack{
 
     }
 
-    public void push(int x) {
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
 
+
+
+    public void push(int x) {
+        stack.push(x);
+        minStack.push(minStack.isEmpty() ? x : Math.min(minStack.peek(),x));
     }
 
     public void pop() {
-
+        stack.pop();
+        minStack.pop();
     }
 
     public int top() {
-
+        return stack.peek();
     }
 
     public int getMin() {
-
+        return minStack.peek();
     }
 }
