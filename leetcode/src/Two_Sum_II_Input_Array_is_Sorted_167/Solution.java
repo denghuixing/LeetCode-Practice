e65fbdba8ed4362e0f8cfbbac777ee5e373479e6
dhuixing <denghuixing@live.com>
2019-06-15 21:23:09 +0800

    init 167 Two Sum II Array is sorted

---
+++ leetcode/src/Two_Sum_II_Input_Array_is_Sorted_167/Solution.java
@@ -0,0 +1,13 @@
+package Two_Sum_II_Input_Array_is_Sorted_167;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.twoSum()));
+    }
+    public int[] twoSum(int[] numbers, int target) {
+
+    }
+}
