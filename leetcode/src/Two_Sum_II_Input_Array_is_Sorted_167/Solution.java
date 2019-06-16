2b5a91c4b29210d04e32ccb9475a92859b961200
dhuixing <denghuixing@live.com>
2019-06-16 10:57:34 +0800

    remove a image hich has ip address

--- leetcode/src/Two_Sum_II_Input_Array_is_Sorted_167/Solution.java
+++ leetcode/src/Two_Sum_II_Input_Array_is_Sorted_167/Solution.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args){
         Solution s = new Solution();
-        System.out.println(Arrays.toString(s.twoSum()));
+        int[] arr = new int[]{2,7,11,15};
+        System.out.println(Arrays.toString(s.twoSum(arr,9)));
     }
     public int[] twoSum(int[] numbers, int target) {
 
