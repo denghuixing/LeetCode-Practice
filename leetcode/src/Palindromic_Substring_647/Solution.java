21164b7389360232e89d42f9c2ead366fe4c3dfd
dhuixing <denghuixing@live.com>
2019-09-03 14:54:42 -0400

    init 647 palindromic substring

---
+++ leetcode/src/Palindromic_Substring_647/Solution.java
@@ -0,0 +1,12 @@
+package Palindromic_Substring_647;
+
+public class Solution {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        int res = s.countSubstrings("aaa");
+    }
+
+    public int countSubstrings(String s) {
+
+    }
+}
