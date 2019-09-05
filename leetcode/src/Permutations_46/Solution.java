a0cd047e567655502c1a0d3354836c91d631f205
dhuixing <denghuixing@live.com>
2019-09-04 21:17:22 -0400

    init 46 permutations

---
+++ leetcode/src/Permutations_46/Solution.java
@@ -0,0 +1,13 @@
+package Permutations_46;
+
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args){
+        Solution s = new Solution();
+
+    }
+    public List<List<Integer>> permute(int[] nums) {
+
+    }
+}
